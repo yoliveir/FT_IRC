267d71e2170b9cfc2293637348b7fc3800ab4859
Paolo Sapio <psapio@c1r16s1.42madrid.com>
2026-06-26 18:00:44 +0200

    est´á roto

--- socket_test/server.cpp
+++ socket_test/server.cpp
@@ -19,7 +19,7 @@ int	create_socket(void)
 	srand(time(NULL));
 	int port = rand() % 100;
 	address.sin_port = htons(8000 + port); //METER UN PUERTO RANDOM PARAA QUE FALLE MENOS AL CERRAR CON CTRL+C SIN LIMPIAR LOS PUERTOS!!! ESTO ES UN APAÑO CUTRE NO HACER ASI EN EL PROYECTOOOOOO
-	std::cout << "PORT:" << (port + 8000) << "\n";
+	std::cerr << "PORT:" << (port + 8000) << "\n";
 	if (bind(fd_socket, (const struct sockaddr *)&address, sizeof(sockaddr_in)) == -1)
 	{
 		std::cout << "COULD NOT BIND...\n";
@@ -30,27 +30,37 @@ int	create_socket(void)
 }
 #include <cstring>
 #include <stdio.h>
+#define FD_SOCKET_SERVER 0
+
 int	main()
 {
 	int fd_socket = create_socket();
 
 	struct sockaddr_in	peer_address;
-	socklen_t			peer_address_sizeof;
+	memset(&peer_address, '\0', sizeof(peer_address));
+	socklen_t			peer_address_sizeof  = (socklen_t)sizeof(sockaddr_in); //es una variable de 32bit! (tipo int)
+	
 	int fd_client = 0;
+
+	int total_sockets_fd = 1;
+
+	unsigned char data[10000];
+	std::memset(data, '\0', sizeof(data));
+	struct pollfd poll_candidate[10]; // {"FD de donde eschucha", EVENT info binaria del qeu tipo quere leer, REVENTS es }
 
 	while (1)
 	{
-		unsigned char data[1000];
-		std::memset(data, '\0', sizeof(data));
-		struct pollfd poll_candidate; // {"FD de donde eschucha", EVENT info binaria del qeu tipo quere leer, REVENTS es }
 
-		poll_candidate.fd = fd_socket;
-		poll_candidate.events = POLLIN;
-		int read_pending_count = poll(&poll_candidate, 1, 10);
-		//std::cout << poll_candidate.revents << "\n";
-		if (poll_candidate.revents ==  POLLIN)
+		poll_candidate[FD_SOCKET_SERVER].fd = fd_socket;
+		poll_candidate[FD_SOCKET_SERVER].events = POLLIN;
+		int read_pending_count = poll(poll_candidate, total_sockets_fd, 10);
+		std::cout << "read_pending_count: " << read_pending_count << "\n";
+
+		if (poll_candidate[FD_SOCKET_SERVER].revents ==  POLLIN)
 		{
-			fd_client = accept(fd_socket, NULL, &peer_address_sizeof);
+			fd_client = accept(fd_socket, (struct sockaddr *)&peer_address, &peer_address_sizeof);
+			
+			poll_candidate[total_sockets_fd++].fd = fd_client;
 			if (fd_client == -1)
 			{
 				perror("aaaaaaaaaaaah: "); 
@@ -60,17 +70,21 @@ int	main()
 			std::cout << inet_ntoa(peer_address.sin_addr) << " conection accepted\n";
 			send(fd_client, "BIENVENIDO AL SERVIDOR\n", 23, 0);
 		}
-		if (fd_client == -1)
+
+		for (int i = 1; i < total_sockets_fd; i++)
 		{
-			std::cerr << "EL CLIENTE NO SE HA PODIDO CONECTAAAAR\n";
-			fd_client = 0;
+			printf("total_sockets_fd:%d\n", total_sockets_fd);
+			//printf("--> ii = %d  - next_candidate_iindex: %d\n", i, total_sockets_fd);
+			//std::cout <<  "_" << poll_candidate[i].revents << "_\n";
+			if (poll_candidate[i].events > 0)
+			{
+				printf("i: %d\n", i);
+				printf("poll_candidate[i].fd: %d\n", poll_candidate[i].fd);
+				recv(poll_candidate[i].fd, data, 1000, 0);
+				std::cout <<  data << "\n";
+				//read(0, data, 1000);
+			}
 		}
-		if (fd_client > 0) {
-			send(fd_client, "ESTAS EN EL SERVIDOR\n", 22, 0);
-			recv(fd_client, data, 1000, 0);
-			std::cout <<  data << "\n";
-			//read(0, data, 1000);
-		}
-		usleep(100000);
+		usleep(1000000);
 	}
 }
